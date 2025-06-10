55a9b09f967a53eaefd3c478a614cc334e633d02
Roman L <roman-ronin@yandex.ru>
2025-06-10 22:59:31 +0300

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ter-homeworks-main-03
-ter-homeworks-main-03
+
+Задание 1
+
+cd /mnt/c/Users/rlyst/Netology/ter-homeworks-main-03/src
--- src/variables.tf
+++ src/variables.tf
@@ -1,16 +1,19 @@
 ###cloud vars
-variable "token" {
-  type        = string
-  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
-}
+
+#variable "token" {
+#  type        = string
+#  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+#}
 
 variable "cloud_id" {
   type        = string
+  default     = "b1g2uh898q9ekgq43tfq"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default     = "b1g22qi1cc8rq4avqgik"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
